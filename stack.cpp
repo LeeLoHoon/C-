88a13f590e6833bdc9bb4672fcd913b67fedc27f
leelohoon <21600558@handong.edu>
2019-05-02 13:51:40 +0900

    stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
-#include "stack.h" 
-
-Stack::Stack()
-{
-	lenth=0;
-   topNode = NULL;
-} 
-
-void Stack::Push(Element item)
-{
-	stack_node *new_node = new stack_node;
-	if(new_node == NULL)
-		return;
-	new_node->data =item;
-	new_node->link = topNode;
-	topNode = new_node;
+#include "stack.h"
+  
+Stack::Stack(){
+  length=0;
+  topNode = NULL;
 }
-Element Stack::Pop()
-{	Element e;
-	stack_node *del;
-	if(topNode == NULL)
-		return (Element)0;
-	e=topNode->data;
-	del = topNode;
-	topNode = topNode ->link;
-	delete del;
-	return e;
-} 
+
+void Stack::Push(Element item){
+  stack_node *new_node = new stack_node;
+  new_node->data =item;
+  new_node->link = topNode;
+  topNode = new_node;
+  length++;
+}
+
+Element Stack::Pop(){  
+  Element e;
+  stack_node *del;
+  if(topNode == NULL)
+    return e;
+  e=topNode->data;
+  del = topNode;
+  topNode = topNode ->link;
+  delete del;
+  cout<<e.id<<", "<<e.name<<", "<<e.email<<endl;
+  return e;
+ }
